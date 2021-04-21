a2b157b512b847ccaf87fca18e1e06fb5f3a4977
Reesh Miranda <rtmiranda18@gmail.com>
2021-04-21 22:33:41 +0800

    applied front end applications

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,11 @@
 const path = require('path');
+const express = require('express');
 
 module.exports = (app) => {
+    app.use(express.static(__dirname + '/../pages/assets/css'));
+
+    app.use(express.static(__dirname + '/../pages/assets/js'));
+
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname + '/../pages/index.html'));
     })
