8413b5ea64b2827d87ed9eae3923d207166a95e0
Reesh Miranda <rtmiranda18@gmail.com>
2021-04-23 12:32:10 +0800

    reworked apiRoutes.js to be able to read, add and delete notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,42 @@
-const noteData = require('../db/db.json');
-
+const path = require('path');
+const noteData = require('../database/db.json');
+const express = require('express');
+const fs = require('fs');
 module.exports = (app) => {
+    app.use(express.static(__dirname + '/../database/'));
     // Read Notes
-    app.get('/api/notes', (req, res) => res.json(noteData));
-    // Add Notes
+    app.get('/api/notes', (req, res) => {
+        res.json(noteData);
+    });
+    app.get('/api/notes/:id', (req, res) => res.json(noteData));
+    
+    // Add Note
     app.post('/api/notes', (req, res) => {
         res.send('Got a POST request');
-        noteData.push(req.body);
+        fs.readFile('./database/db.json',function(err, content){
+            noteData.push(req.body);
+            var notes = JSON.parse(content);
+            notes.push(req.body);
+            if(err) {
+                return console.log(err);
+            }
+            console.log(notes);
+            fs.writeFile('./database/db.json', JSON.stringify(notes), function(err, data) {
+                console.log(data);
+                if(err) {
+                    return console.log(err);
+                }
+                console.log("The file was saved!");
+            });
+        })
         res.json(true);
     })
+
+    // Delete Note
     app.delete('/api/notes/:id', (req, res) => {
         res.send('Got a delete request');
-        console.log("receivedbyAPI");
+        const note = req.body
+        console.log(note);
         console.log(res);
         res.json(true);
     })
